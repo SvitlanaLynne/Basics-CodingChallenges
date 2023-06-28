d6aa4e65119f42a840ea42bbf2f9ddf473a0b6ba
Svitlana <Svetlanalynne@gmail.com>
2023-06-28 12:29:35 -0300

    mostFrequent added

--- arrayChunk.js
+++ arrayChunk.js
@@ -5,20 +5,23 @@ const myArr = ["apple", "sunshine", "book", "mountain", "ocean", "laughter", "gu
 
 
 function Chunk(arr, chunkSize) {
+
     let result = [];
+
     if (arr.length > chunkSize) {
+
         while (arr.length >= chunkSize) {
             let chunk = arr.slice(0, chunkSize);
             result.push(chunk);
             arr.splice(0, chunkSize);
             if (arr.length < chunkSize && arr.length !== 0) {
                 console.log("The remainder:", arr);
-            }
-        }
+            };
+        };
         console.log(result)
     } else {
         console.log("chunk size exceeds the array's length")
-    }
-}
+    };
+};
 
-Chunk(myArr, 2);
+Chunk(myArr, 3);
