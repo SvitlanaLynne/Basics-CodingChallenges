124c4257c1906bae5f0b0316bd4f0b51d3187847
Svitlana <Svetlanalynne@gmail.com>
2023-12-07 12:41:04 -0400

    string split in progress

--- dataStringSplit.js
+++ dataStringSplit.js
@@ -3,143 +3,123 @@
 // (x= min element of an increasing sequence. if multiple, take the min)
 
 // const s = "1234";
-// const s = "99100101";
+const s = "99100101";
 // const s = "91011";
 // const s = "1920212223";
 // const s = "444444454446";
 // const s = "110111112";
 // const s = "101102103";
 // const s = "121314";
-const s = "123124125";
+// const s = "123124125";
+// const s = "1819212223";
+// const s = "2456";
+// const s = "67686869100";
+
+// const s = "7";
+// const s = "101103";
+// const s = "010203";
+// const s = "13";
+// const s = "1";
+// const s = "91112";
+// const s = "10203";
+
+// const s = "99910001001";
+// const s = "7891011";
+// const s = "9899100";
+// const s = "999100010001";
 
 function splitString(s) {
+  //Initial Array
   let arr = s.split("").map((x) => parseInt(x));
   console.log("initial array:", arr);
 
-  let first = arr[0];
-  let decimals = 1;
-  let boolArr = [];
+  // Starts with "0" or Single element
+  if (arr[0] == 0 || arr.length === 1) {
+    console.log(`NO`);
+    return;
+  }
 
-  for (let d = 1; d <= decimals; d++) {
+  // Loop until the result is found (true or false)
+  let beautiful;
+
+  while (beautiful !== true && beautiful !== false) {
+    let boolArr = [];
     for (let i = 0; i < arr.length - 1; i++) {
       if (arr[i + 1] - arr[i] === 1) {
-        console.log("first in the sequence:", first);
+        boolArr.push(true);
       } else {
-        decimals++;
-        reassemble(decimals);
-        break;
+        // check if the current is critical
+        if (isCritical(arr[i])) {
+          const tryArr = testCriticalNext(arr[i], s);
+
+          // is next reassembled element is 10...?
+          console.log("TryArr", tryArr);
+          if (tryArr[0] - arr[i] === 1) {
+            arr = tryArr;
+            break;
+          }
+        } else {
+          // try more digits
+          console.log("stuck on:", arr[i], "reassembling...");
+          const digits = i + 1;
+          let reassambledArr = reassamble(arr[i], digits, s);
+          // ---- NO, did not help. Check if reassembling changed anything OR did not find the starting point and reassembled all in one piece
+          if (reassambledArr.length === arr.length || arr[0].toString() === s) {
+            beautiful = false;
+            boolArr.push(false);
+            break;
+          } else {
+            // ---- YES, helped.
+            arr = reassambledArr;
+            console.log("REASSEMBLED ARR:", arr);
+          }
+        }
       }
     }
-  }
-
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i + 1] - arr[i] === 1) {
-      boolArr.push(true);
+    // all elements are checked, beautiful is...
+    if (boolArr.includes(false)) {
+      beautiful = false;
+      console.log("NO");
     } else {
-      boolArr.push(false);
+      beautiful = true;
+      console.log(`YES ${arr[0]}`);
     }
+    return beautiful;
   }
 
-  console.log("boolArr", boolArr);
-
-  boolArr.includes(false) ? console.log(`NO`) : console.log(`YES${first}`);
-
-  function reassemble(step) {
-    // assemble first element
-    first = parseInt(s.slice(0, step));
-    console.log("\nFIRST", first);
-    console.log("decimals at this stage", decimals);
-
-    // check if 1st ends with 9
-    // let char = "9";
-    // let critical = parseInt(char.repeat(decimals));
-    // console.log("CRITICAL", critical);
-
-    // if (first === "9" || first === critical) {
-    //   step++;
-    //   console.log(decimals, step);
-    // }
-
-    // slice the tail
-    let tail = s.slice(first.toString().length);
-    console.log("TAIL", tail);
-    let tailArr = [];
-    for (let i = 0; i < tail.length; i += step) {
-      tailArr.push(tail.slice(i, i + step));
-    }
-    // tailArr.shift();
-    console.log("tailArr", tailArr);
-
-    // glue first element and tail
-    arr = [first, ...tailArr];
-    console.log("REASSEMBLED ARR:", arr);
+  function testCriticalNext(num, s) {
+    let digits = num.toString().length;
+    const reassembledArr = reassamble(digits, digits + 1, s); // returns an array, where the num is the first elemnt, the rest is sliced with more digits
+    reassembledArr.shift();
+    return reassembledArr; // returns the sliced tail
   }
 
-  // check if ends with 9
-  // let firstStr = first.toString();
-  // let lastChar = firstStr[firstStr.length - 1];
-  // console.log(lastChar);
+  function isCritical(num) {
+    const string = num.toString();
+    const splitArr = string.split("");
+    return splitArr.every((x) => x === "9"); // returns true or false
+  }
 
-  // if 9 in the first
-  // let step = decimals;
-  // if (arr[0] === 9 || lastChar === "9") {
-  //   step = decimals + 1;
-  // }
-  // split to array
+  function reassamble(breakpoint, step, s) {
+    const nose = s.slice(0, breakpoint); // from beginning, including
+    const tail = s.slice(breakpoint); // not including breakpoint
+    const noseNum = parseInt(nose);
+
+    const tailArr = [];
+    for (i = 0; i < tail.length; i += step) {
+      tailArr.push(parseInt(tail.slice(i, i + step)));
+    }
+    return [noseNum, ...tailArr]; // returns a new array
+  }
 }
 
 // console.log(splitString(s));
 splitString(s);
 
-// for (let i = 0; i <= arr.length; i++) {
-//     // joined 9-s
-//     while (arr[i] === 9) {
-//       if (arr[i + 1] === 9) {
-//         arr[i] = arr[i] * 10 + arr[i + 1];
-//         arr.splice(i + 1, 1);
-//       }
-//       i++;
-//     }
-//
+// duplicates
+// if (first === 9) {
+//   let i = 0;
+//   while (arr[i + 1] === 9) {
+//     first = first * 10 + 9;
+//     i++;
 //   }
-
-// checkFirstTwo(arr);
-
-// function checkFirstTwo(arr) {
-//   let boolArr = [];
-//   for (i = 0; i < arr.length; i++) {
-//     if (arr[i + 1] - arr[i] === 1) {
-//       boolArr.push(true);
-//     } else {
-//       console.log("broke on:", arr[i]);
-//       boolArr.push(false);
-//       decimals = boolArr.length;
-//       console.log("its decimals:", decimals);
-//       reassemble(decimals);
-//       break;
-//     }
-//   }
-//   console.log("boolArr", boolArr);
-//   console.log("first element:", first);
-// }
-
-// for (let d = 1; d < 6; d++) {
-//   if (arr[1] - arr[0] === 1) {
-//     console.log("assume first in the sequence:", first);
-//     break;
-//   } else {
-//     decimals++;
-//     reassemble(decimals);
-//   }
-// }
-
-// if duplicate
-// if (arr[0] === arr[1]) {
-//   for (let j = 1; j < arr.length; j++) {
-//     while (arr[0] === arr[j]) {
-//       first = first * 10 + arr[j];
-//       j++;
-//       decimals++;
-//     }
-//   }
-// }
