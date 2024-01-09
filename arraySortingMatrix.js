f020b1952924ded5b2e57922f697c20ea9cf38d7
Svitlana <Svetlanalynne@gmail.com>
2024-01-09 10:51:56 -0400

    sorted Matrix (grid) added

--- arraySortingMatrix.js
+++ arraySortingMatrix.js
@@ -4,44 +4,21 @@
 // const arr = ["ebacd", "fghij", "olmkn", "trpqs", "xywuv"];
 const arr = ["abc", "ade", "efg"];
 
-grid
-  .map((x) => x.split(""))
-  .map((elem) => elem.sort((a, b) => a.localeCompare(b)));
+function isSortedDesc(grid) {
+  const sorted = grid
+    .map((string) => string.split(""))
+    .map((subArr) => subArr.sort((a, b) => a.localeCompare(b)).join(""));
 
-const rowsStart = grid.map((x) => x.slice(0, 1)).join("");
-const sortedRows = rowsStart
-  .split("")
-  .sort((a, b) => a.localeCompare(b))
-  .join("");
+  const strLength = grid[0].length;
 
-return rowsStart === sortedRows ? "YES" : "NO";
-
-// function isSortedDesc(arr) {
-//   arr.map((x) => x.split("")).map((elem) => elem.sort());
-
-//   const rowsStart = arr.map((x) => x.slice(0, 1)).join("");
-//   const sortedRows = rowsStart.split("").sort().join("");
-
-//   return rowsStart === sortedRows ? "YES" : "NO";
-// }
-
-// function isSortedDesc(arr) {
-//   arr.map((x) => x.split("")).map((elem) => elem.sort());
-
-//   const rowsStart = arr.map((x) => x.slice(0, 1)).join("");
-//   const sortedRows = rowsStart.split("").sort().join("");
-
-//   return rowsStart === sortedRows ? "YES" : "NO";
-// }
-// function isSortedDesc(arr) {
-//   const splitArr = arr.map((x) => x.split(""));
-//   const sortedElemArr = splitArr.map((elem) => elem.sort());
-
-//   let firstChars = [];
-//   sortedElemArr.map((elem) => firstChars.push(elem[0])).join("");
-//   const sortedFirstChars = firstChars.sort().join("");
-
-//   return firstChars !== sortedFirstChars ? "YES" : "NO";
-// }
-
-console.log(isSortedDesc(arr));
+  for (let j = 0; j < strLength; j++) {
+    for (let i = 0; i < strLength - 1; i++) {
+      let current = sorted[i].charAt(j);
+      let next = sorted[i + 1].charAt(j);
+      if (current > next) {
+        return "NO";
+      }
+    }
+  }
+  return "YES";
+}
