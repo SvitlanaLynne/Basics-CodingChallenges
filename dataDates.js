7154a72c5b066aa412e25d8ceff3c6d581fee197
Svitlana <Svetlanalynne@gmail.com>
2024-01-16 10:00:39 -0400

    finding lucky dates added

--- dataDates.js
+++ dataDates.js
@@ -1,75 +1,68 @@
 // given 2 dates, transform all dates between them into integeres concatting months and years only
-// print  those integers, which can be divided by 4 or 7
+// print  the Number of integers, which can be divided by 4 or 7
 
-const str = "10-08-2001 15-09-2002";
+const input = "02-08-2025 04-09-2025";
+// const str = "10-08-2001 15-09-2002";
 
-function checkLucky(str) {
+function checkLucky(input) {
   //convert to needed format
-  const uv = str.split(" ");
+  const uv = input.split(" ");
   const formattedUV = uv.map((x) => {
     const parts = x.slice(0, 5).split("-");
     const year = x.slice(-4);
     return `${parts[1]}-${parts[0]}-${year}`;
   });
-  console.log(uv);
-  console.log("formatted uv", formattedUV);
+  // console.log(uv);
+  // console.log("formatted uv", formattedUV);
 
   // dates array
   let startDate = new Date(formattedUV[0]);
   const endDate = new Date(formattedUV[1]);
 
   let datesArr = [];
+  const oneDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
+
   while (startDate <= endDate) {
     const year = startDate.getFullYear();
-    const month = startDate.getMonth() + 1; // Adding 1 because getMonth() returns zero-based index
+    const month = startDate.getMonth() + 1;
+    const day = startDate.getDate();
 
-    datesArr.push(`${month}-${year}`);
-    startDate.setMonth(startDate.getMonth() + 1);
+    datesArr.push(`${day}-${month}-${year}`);
+    startDate.setTime(startDate.getTime() + oneDay);
   }
 
-  console.log("dates array", datesArr);
+  // console.log("dates array", datesArr);
 
   // numbers array
   const numArr = datesArr.map((x) => {
-    const [m, y] = x.split("-");
-    return parseInt(m + y);
+    let [d, m, y] = x.split("-");
+    if (m.length < 2) {
+      m = "0" + m;
+    }
+    return parseInt(d + m + y);
   });
 
-  //
+  // Check 4 and 7
+  let count = 0;
+  for (const elem of numArr) {
+    if (elem % 4 === 0 || elem % 7 === 0) {
+      count++;
+    }
+  }
 
-  return numArr;
+  console.log(count);
+  return count;
 }
 
-console.log(checkLucky(str));
+console.log(checkLucky(input));
 
-// function isDivisibleBy7(number) {
-//   // Convert the number to a string to determine its length
-//   const numStr = number.toString();
-//   const length = numStr.length;
+const reverseDayAndMonth = (dateStr) => {
+  let parts = dateStr.split("-");
+  let day = parts[0];
+  let month = parts[1];
+  let year = parts[2];
 
-//   // Check if the length is even or odd
-//   if (length % 2 === 0) {
-//     // If the length is even, take two digits from the middle
-//     const middleDigits = parseInt(numStr.substr(length / 2 - 1, 2), 10);
-//     const remainingDigits = parseInt(
-//       numStr.substr(0, length / 2 - 1) + numStr.substr(length / 2 + 1),
-//       10
-//     );
+  const newDateStr = month + "-" + day + "-" + year;
 
-//     return (remainingDigits - middleDigits) % 7 === 0;
-//   } else {
-//     // If the length is odd, take one digit from the middle
-//     const middleDigit = parseInt(numStr.charAt(length / 2), 10);
-//     const remainingDigits = parseInt(
-//       numStr.substr(0, length / 2) + numStr.substr(length / 2 + 1),
-//       10
-//     );
-
-//     return (remainingDigits - middleDigit) % 7 === 0;
-//   }
-// }
-
-// // Example usage with a 4-digit number
-// const num = 4132;
-
-// console.log(`${num} is divisible by 7: ${isDivisibleBy7(num)}`);
+  return newDateStr;
+};
