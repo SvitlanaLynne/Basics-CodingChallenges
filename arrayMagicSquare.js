c449c2883a110580d1b485142134a8891bc28310
Svitlana <Svetlanalynne@gmail.com>
2024-04-25 12:29:14 -0300

    magic square in progress

--- arrayMagicSquare.js
+++ arrayMagicSquare.js
@@ -7,11 +7,17 @@
 //   [3, 5, 7],
 //   [8, 1, 5],
 // ];
+
 const s = [
   [5, 3, 4],
   [1, 5, 8],
   [6, 4, 2],
 ];
+// const s = [
+//   [8, 3, 4],
+//   [1, 5, 9],
+//   [6, 7, 2],
+// ];
 
 function toMagicSquareCost(s) {
   // form rows, columns, diagonals for checking
@@ -26,37 +32,55 @@ function toMagicSquareCost(s) {
   const d1 = [s[0][0], s[1][1], s[2][2]];
   const d2 = [s[0][2], s[1][1], s[2][0]];
 
+  const Sums = (matrix) => {
+    const arrays = {
+      a1: a1,
+      a2: a2,
+      a3: a3,
+      b1: b1,
+      b2: b2,
+      b3: b3,
+      d1: d1,
+      d2: d2,
+    };
+    let Sums = {};
+    for (let key in arrays) {
+      // calc the sum of a line
+      if (arrays.hasOwnProperty(key)) {
+        const arrayName = key;
+        const array = arrays[key];
+        let sum = 0;
+        sum = array.reduce((total, current) => total + current);
+        // store its frequency
+        if (Sums[sum]) {
+          Sums[sum]++;
+        } else {
+          Sums[sum] = 1;
+        }
+      }
+    }
+
+    return Sums;
+  };
+
   // ----- EARLY - initial matrix is megical: return 0 as a cost
-  let SumsArr = [];
-  if (isMagical(s)) {
+  const SumsResult = Sums(s);
+  if (Object.keys(SumsResult).length === 1) {
     return 0;
   } else {
     // ----- ANALYSE DATA
-    // number of lines Off
-    let counts = {};
-    for (let elem of SumsArr) {
-      if (counts[elem]) {
-        counts[elem]++;
-      } else {
-        counts[elem] = 1;
-      }
-    }
-    console.log("counts of numbers Off", counts);
-
+    console.log("Sums:", SumsResult);
     // max  frequent among sums
-    let maxFrequent;
     let max = 0;
-    for (let elem in counts) {
-      if (counts[elem] > max) {
-        maxFrequent = parseInt(elem);
-        max = counts[elem];
+    for (let elem in SumsResult) {
+      if (SumsResult[elem] > max) {
+        max = +SumsResult[elem];
       }
     }
-    console.log("max Frequent sum is", maxFrequent);
+    console.log("Max is", max);
 
     // ----- EVALUATE INITIAL STATE
-    // find the difference between aimed and current sums
-    // filter those, which are not max
+    // idfentify those, which are not max
     const filtered = SumsArr.filter((x) => x !== parseInt(maxFrequent));
     const numberOfSumsOff = filtered.length;
 
@@ -113,88 +137,8 @@ function toMagicSquareCost(s) {
     // Gather potential answers (difference between the initial and current,adjusted element)
     // pick and output the minimum
   }
-
-  // function isMagical(matrix) {
-  //   // ----- PREPARE DATA  - SUMS
-  //   // Reduce - sum of all elements - got an Array and Object of sums
-  //   const arrays = {
-  //     a1: a1,
-  //     a2: a2,
-  //     a3: a3,
-  //     b1: b1,
-  //     b2: b2,
-  //     b3: b3,
-  //     d1: d1,
-  //     d2: d2,
-  //   };
-  //   const SumsObj = {};
-
-  //   for (let key in arrays) {
-  //     if (arrays.hasOwnProperty(key)) {
-  //       const arrayName = key;
-  //       const array = arrays[key];
-  //       const sum = array.reduce((total, current) => total + current);
-  //       SumsObj[arrayName] = sum;
-  //     }
-  //   }
-
-  //   console.log("Sums:", SumsObj);
-
-  //   const SumsArr = [];
-  //   for (let elem in SumsObj) {
-  //     SumsArr.push(SumsObj[elem]);
-  //   }
-
-  //   const set = [...new Set(SumsArr)];
-  //   if (set.length === 1) {
-  //     console.log("The square is MAGICAL");
-  //     return true;
-  //   }
-  // }
 }
 
 // console.log(toMagicSquareCost(s));
 
-function isNMagical(matrix) {
-  const a1 = [s[0][0], s[0][1], s[0][2]];
-  const a2 = [s[1][0], s[1][1], s[1][2]];
-  const a3 = [s[2][0], s[2][1], s[2][2]];
-
-  const b1 = [s[0][0], s[1][0], s[2][0]];
-  const b2 = [s[0][1], s[1][1], s[2][1]];
-  const b3 = [s[0][2], s[1][2], s[2][2]];
-
-  const d1 = [s[0][0], s[1][1], s[2][2]];
-  const d2 = [s[0][2], s[1][1], s[2][0]];
-  const arrays = {
-    a1: a1,
-    a2: a2,
-    a3: a3,
-    b1: b1,
-    b2: b2,
-    b3: b3,
-    d1: d1,
-    d2: d2,
-  };
-  let sumsFreq = {};
-  for (let key in arrays) {
-    // calc the sum of a line
-    if (arrays.hasOwnProperty(key)) {
-      const arrayName = key;
-      const array = arrays[key];
-      let sum = 0;
-      sum = array.reduce((total, current) => total + current);
-      // store its frequency
-      if (sumsFreq[sum]) {
-        sumsFreq[sum]++;
-      } else {
-        sumsFreq[sum] = 1;
-      }
-    }
-  }
-
-  console.log("Sums frequency is ", sumsFreq);
-  return Object.keys(sumsFreq) !== 1 ? false : true;
-}
-
-console.log(isNMagical(s));
+console.log(toMagicSquareCost(s));
